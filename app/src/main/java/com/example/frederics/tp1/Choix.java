07f1f3947ae3d7e22999500fd138fb289d33a142
FredSWR <frederic.sewera@laposte.net>
2017-11-24 14:38:40 +0100

    Backup
    Backup

--- app/src/main/java/com/example/frederics/tp1/Choix.java
+++ app/src/main/java/com/example/frederics/tp1/Choix.java
@@ -1,8 +1,28 @@
 package com.example.frederics.tp1;
 
-/**
- * Created by frederics on 13/11/2017.
- */
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class Choix {
+public class Choix extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choix);
+
+        Button boutonAnnuler = findViewById(R.id.Annuler);
+        boutonAnnuler.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        Intent boutonAnnuler = new Intent(Choix.this, MainActivity.class);
+        startActivity(boutonAnnuler);
+        finish();
+
+    }
 }
